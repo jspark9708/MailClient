6b69d32349612588e205c515ddbd4c05e73d0ef2
JSPARK <55075290+ParkJae-sung@users.noreply.github.com>
2023-03-01 16:06:29 +0900

    clean code - 2
    - 전체적인 주석정리
    - MIME 부분 임시 추가

--- SMTP/src/MailGUI.java
+++ SMTP/src/MailGUI.java
@@ -1,43 +1,44 @@
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.Choice;
-import javax.swing.JButton;
+
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")//The serializable class OboeObject does not declare a static final serialVersionUID field of type long 방지
 public class MailGUI extends JFrame {
-	
-	
-	private JPanel contentPane;
-	private JTextField senderField;
-	private JLabel receiverForm;
-	private Choice pwField;
-	private JTextField receiverField;
-	private JLabel subjectForm;
-	private JTextField subjectField;
-	private JLabel bodyForm;
-	private JTextField bodyField;
 
-	public MailGUI() {
-		
-		setTitle("메일 전송");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		setBounds(100, 100, 700, 600);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    private JPanel contentPane;
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+    private JTextField senderField;
+    private JLabel receiverForm;
+    private Choice pwField;
+    private JTextField receiverField;
+    private JLabel subjectform;
+    private JTextField subjectField;
+    private JLabel bodyForm;
+    private JTextArea bodyField;
 
-		/*From 라벨*/
-		JLabel senderForm = new JLabel("보내는 사람");
+    /**
+     * Create the frame.
+     */
+    public MailGUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 700, 600);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+
+        /*From 라벨*/
+        JLabel senderForm = new JLabel("보내는 사람");
         senderForm.setFont(new Font("굴림", Font.PLAIN, 15));
         senderForm.setBounds(35, 35, 100, 30);
         contentPane.add(senderForm);
@@ -45,14 +46,13 @@ public class MailGUI extends JFrame {
         /*발신메일 입력 필드*/
         senderField = new JTextField();
 
-        senderField = new JTextField("your-email-here@gmail.com");
         senderField.setBounds(150, 35, 200, 30);
         contentPane.add(senderField);
         senderField.setColumns(10);
         senderField.setEditable(true);
-        
+
         /*비밀번호 입력 필드*/
-        //choice를 통해서 비밀번호 찾아 일일히 입력하는 과정 간략
+        //choice를 통해서 비밀번호 찾아 일일히 입력하는 과정 간략하게
         pwField = new Choice();
         pwField.setBounds(350, 35, 200, 30);
         pwField.setFont(new Font("굴림", Font.PLAIN, 20));
@@ -62,6 +62,7 @@ public class MailGUI extends JFrame {
         pwField.add("second password");
         pwField.add("third password");
         pwField.add("forth password");
+        
         add(pwField);
 
         /*To 라벨*/
@@ -72,37 +73,36 @@ public class MailGUI extends JFrame {
 
         /*수신메일 입력 필드*/
         receiverField = new JTextField();
-        receiverField = new JTextField("receiver-email-here@naver.com");
         receiverField.setColumns(10);
-        receiverField.setBounds(150, 85, 400, 30);
+        receiverField.setBounds(150, 88, 400, 30);
         contentPane.add(receiverField);
 
-        /*제목 라벨*/
-        subjectForm = new JLabel("제목");
-        subjectForm.setFont(new Font("굴림", Font.PLAIN, 15));
-        subjectForm.setBounds(35, 135, 100, 30);
-        contentPane.add(subjectForm);
+        /*제목 표시*/
+        subjectform = new JLabel("제목");
+        subjectform.setFont(new Font("굴림", Font.PLAIN, 15));
+        subjectform.setBounds(35, 145, 100, 30);
+        contentPane.add(subjectform);
 
-        /*제목 입력 필드*/
+        /*제목 입력 받을필드*/
         subjectField = new JTextField();
         subjectField.setColumns(10);
-        subjectField.setBounds(150, 135, 400, 30);
+        subjectField.setBounds(150, 145, 400, 30);
         contentPane.add(subjectField);
 
-        /*Body 라벨*/
+        /*Body 표시*/
         bodyForm = new JLabel("내용");
         bodyForm.setFont(new Font("굴림", Font.PLAIN, 15));
         bodyForm.setBounds(35, 211, 100, 30);
         contentPane.add(bodyForm);
 
-        /*내용 입력 필드*/
-        bodyField = new JTextField();
+        /*내용 입력받을 필드*/
+        bodyField = new JTextArea();
         bodyField.setColumns(10);
         bodyField.setBounds(150, 213, 400, 272);
         contentPane.add(bodyField);
 
         /*전송 버튼*/
-        JButton sendButton = new JButton("전송");
+        JButton sendButton = new JButton("메일 보내기");
 
         sendButton.setBounds(280, 517, 150, 23);
         contentPane.add(sendButton);
@@ -110,31 +110,30 @@ public class MailGUI extends JFrame {
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
+                String From = senderField.getText();
+                String PW = pwField.getSelectedItem();
+                String To = receiverField.getText();
+                String Subject = subjectField.getText();
+                String Body = bodyField.getText();
 
-            	String From = senderField.getText();
-            	String PW = pwField.getSelectedItem();
-            	String To = receiverField.getText();
-            	String Subject = subjectField.getText();
-            	String Body = bodyField.getText();
-               
-            	if (From.length() *  To.length() * Body.length() == 0) { //Subject는 없어도 <제목없음>으로 전송되므로
-            		JOptionPane.showMessageDialog(null, "메일 폼을 작성해주세요!");
-            	}
-               
-            	try{
-            		MailServer.MailSender(From, PW, To, Subject, Body);
-            		JOptionPane.showMessageDialog(null, "메일 보내기 성공!", "메일 전송 상태", JOptionPane.PLAIN_MESSAGE);
-            		System.out.println("==========================");
-            		System.out.println("메일 보내기 성공!");
-            	}
-            	catch(Exception e){
-            		JOptionPane.showMessageDialog(null, "메일이 발송되지 않았습니다.", "메일 전송 상태", JOptionPane.ERROR_MESSAGE);
-            		System.out.println("==========================");
-            		System.out.println("메일이 발송되지 않았습니다.");
-            		System.out.println(e.toString());
-            	}
-           	}
-            
+                if (From.length() *  To.length() * Body.length() == 0) { //Subject는 없어도 <제목없음>으로 전송되므로
+                    JOptionPane.showMessageDialog(null, "메일 폼을 작성해주세요!");
+                }
+
+                try{
+                    MailServer.MailSender(From, PW, To, Subject, Body);
+                    JOptionPane.showMessageDialog(null, "메일 보내기 성공!");
+                    System.out.println("==========================");
+                    System.out.println("메일 보내기 성공!");
+                    bodyField.setText("");
+                    subjectField.setText("");
+                }catch(Exception e){
+                    JOptionPane.showMessageDialog(null, "메일 보내기 실패!");
+                    System.out.println("==========================");
+                    System.out.println("메일 보내기 실패!");
+                    System.out.println(e.toString());
+                }
+            }
         });
     }
 }
--- SMTP/src/Main.java
+++ SMTP/src/Main.java
@@ -4,20 +4,27 @@ import java.io.*;
 import java.util.Base64;
 
 class MailServer{
-    public static void MailSender(String From, String PW, String To, String Subject, String Data) throws Exception{
+    public static void MailSender(String From, String PW, String To, String Subject, String Body) throws Exception{
 
         String Reply;
 
+        //SSLSocket을 만들기 위해서 SSLSocketFactory를 선언, 기본 SSLSocketFactory를 return 하는 함수
         SSLSocketFactory sslsocketf = (SSLSocketFactory) SSLSocketFactory.getDefault();
         System.out.println("연결 시작");
-        SSLSocket socket = (SSLSocket)sslsocketf.createSocket("smtp.gmail.com", 465);
+        //SSLSocket socket = (SSLSocket) socketFactory.createSocket("smtp.gmail.com", 587);
 
+        //Secure Socket Layer(암호화 기반 인터넷 보안 프로토콜)를 이용하여 gmail smtp 서버, 465 port에 연결
+        SSLSocket socket = (SSLSocket)sslsocketf.createSocket("smtp.gmail.com", 465);
         System.out.println("연결 성공");
 
         //메일을 받는 서버의 응답을 보기위해서 socket에 InputStream 부착
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        //자동 개행을 위해서 PrintWriter 클래스 사용, socket에 부착하여 command를 server에 보내는 역할
         PrintWriter outToServer = new PrintWriter(socket.getOutputStream(), true);
 
+        //연결을 시도하고 응답을 받는다. Status가 220이면 연결 성공
+        //220: The server is ready (response to the client’s attempt to establish a TCP connection)
         Reply = inFromServer.readLine();
         if(Reply.startsWith("220"))
             System.out.println("서버 연결 성공!");
@@ -25,22 +32,23 @@ class MailServer{
             System.out.println("서버 연결 실패");
             System.exit(0);
         }
-
         System.out.println(Reply);
 
-        //HELO command
-        System.out.println("HELO 명령을 전송합니다.");
+        //HELO command: SMTP 세션과 대화를 시작
+        //250: The requested command is completed. As a rule, the code is followed by OK
         outToServer.println("HELO gmail.com");
         Reply = inFromServer.readLine();
         ReplyCheck(Reply, "250");
 
-        //AUTH command
-        //본인 gmail id와 pw 넣기
+        //본인 gmail id와 pw 넣기, Base64로 encoding
         String originalInput = From;
         String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
         String originalInput2 = PW;
         String encodedString2 = Base64.getEncoder().encodeToString(originalInput2.getBytes());
 
+        //AUTH LOGIN command: 서버에 대한 클라이언트 인증, 사용자의 아이디와 암호로 인증
+        //334: Response to the AUTH command when the requested security mechanism is accepted
+        //235: Authentication successful (response to AUTH)
         outToServer.println("AUTH LOGIN");
         Reply = inFromServer.readLine();
         ReplyCheck(Reply, "334");
@@ -51,33 +59,57 @@ class MailServer{
         Reply = inFromServer.readLine();
         ReplyCheck(Reply, "235");
 
-        //MAIL FROM command
+        //MAIL FROM command: 보낸이 메일 지정
         outToServer.println("MAIL FROM:<" + From + ">");
         Reply = inFromServer.readLine();
         ReplyCheck(Reply, "250");
 
-        //RCPT TO command
+        //RCPT TO command: 받는이 메일 지정
         outToServer.println("RCPT TO: <" + To + ">");
         Reply = inFromServer.readLine();
         ReplyCheck(Reply, "250");
-        
-        //DATA command
+
+        //DATA command: 메시지 본문 내용 시작
+        //354: The server confirms mail content transfer (response to DATA). After that, the client starts sending the mail. Terminated with a period ( “.”)
         outToServer.println("DATA");
-        
+        Reply = inFromServer.readLine();
+        ReplyCheck(Reply, "354");
+
         //DATA command 이후에 Subject 보내기위한 header 전송
         String header = "To: " + To + "\r\n";
         header += "From:"+ From +"\r\n";
         header += "Subject:" + hangul(Subject)+"\r\n";
-        outToServer.println(header+"\r\n");
-        
-        Reply = inFromServer.readLine();
-        ReplyCheck(Reply, "354");
-        outToServer.println(Data);
-        outToServer.println(".");
+        header += "MIME-Version: 1.0\r\n";//file attach용
+        header += "Content-Type: multipart/mixed; boundary=KkK170891tpbkKk__FV_KKKkkkjjwq\r\n";
+        outToServer.println(header + "\r\n");
+
+
+        String filename = "info.txt";
+        String filebody = "파일 내용입니다";
+        //String filepath = "info.txt";
+
+        String mimeBody = ("--KkK170891tpbkKk__FV_KKKkkkjjwq\r\n");
+        mimeBody += ("Content-Type: text/plain; charset=\"utf-8\"\r\n\r\n");
+        mimeBody += Body + "\r\n";
+        mimeBody += "\r\n";
+
+        mimeBody += ("--KkK170891tpbkKk__FV_KKKkkkjjwq\r\n");
+        mimeBody += ("Content-Type: text/plain\r\n");
+        mimeBody += ("Content-Transfer-Encoding: utf-8\r\n");
+        mimeBody += ("Content-Disposition: attachment;\r\n filename=" + filename + "\r\n");
+        //TODO 우리가 지정한 파일을 넣을 수 있도록 코드 수정하기
+        //XXX 아무런 설정도 없는데 파일에 내용이 존재함 --> utf로 변환해보니 아래 --KkK170891tpbkKk__FV_KKKkkkjjwq--\r\n 내용이 들어가있음
+        mimeBody += "\r\n";
+        mimeBody += hangul(filebody) + "\r\n";
+        mimeBody += "--KkK170891tpbkKk__FV_KKKkkkjjwq--\r\n";
+        outToServer.println(mimeBody);
+
+        outToServer.println("\r\n.\r\n");
         Reply = inFromServer.readLine();
         ReplyCheck(Reply, "250");
 
-        //QUIT command
+        //QUIT command: 연결 종료
+        //221: The server closes the transmission channel
         outToServer.println("QUIT");
         Reply = inFromServer.readLine();
         ReplyCheck(Reply, "221");
@@ -88,28 +120,30 @@ class MailServer{
         socket.close();
     }
 
-    //응답 코드 확인
+    //response와 status code를 비교하여서 메일 보내는 것이 성공하고 있는지 확인 및 response 출력
     public static void ReplyCheck(String Reply, String Status) throws Exception{
         if(!Reply.startsWith(Status))
             throw new Exception(Reply);
         System.out.println(Reply);
     }
-    
-    //수정 없이 보낼경우 한글파이른 깨지므로, 유니코드 변환
+
+    //수정 없이 보낼경우 한글파일은 깨지므로, 유니코드 변환
     public static String hangul(String Unicodestr) throws UnsupportedEncodingException{
-  	  if(Unicodestr==null) return null;
-  	  return new String(Unicodestr.getBytes("utf-8"));
+        if(Unicodestr==null) return null;
+        return new String(Unicodestr.getBytes("UTF-8"));
     }
+
+    //프로그램 실행
+    public static void run() {
+        try {
+            MailGUI frame = new MailGUI();
+            frame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String args[]){
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    MailGUI frame = new MailGUI();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        MailServer.run();
     }
 }
